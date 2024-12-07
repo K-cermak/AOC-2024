b1c94ea2109fb2c78ebdb55351caf3d5cae36b51
K-cermak <karel4cermak@seznam.cz>
2024-12-07 13:42:56 +0100

    Add day 07

---
+++ day07/07-1.py
@@ -0,0 +1,46 @@
+from itertools import product
+
+def main():
+    with open("day07/07-input.txt", "r") as file:
+        data = file.readlines()
+        data = [line.rstrip() for line in data]
+
+    correct = 0
+    for line in data:
+        parts = line.split(": ")
+        want_result = int(parts[0])
+
+        segments = parts[1].split(" ")
+        numbers = []
+        for item in segments:
+            numbers.append(int(item))
+
+        if(correct_res(numbers, want_result)):
+            correct += want_result
+    
+    print(correct)
+
+def correct_res(numbers, want_result):
+    elements = ['+', '*']
+    combinations = [''.join(comb) for comb in product(elements, repeat=(len(numbers) - 1))]
+
+    for combination in combinations:
+        count = numbers[0]
+
+        for index, number in enumerate(numbers):
+            if index == 0:
+                continue
+
+            if combination[index - 1] == "+":
+                count += number
+            else:
+                count *= number
+            
+        if count == want_result:
+            return True
+
+
+    return False
+
+if __name__ == "__main__":
+    main()
